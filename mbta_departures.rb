20b8195d5426bba613ddbdb74bc7d303944c8f52
Richard Davis <rdavis.bacs@gmail.com>
2013-10-03 15:10:03 -0400

    bootstrap formatting

--- mbta_departures.rb
+++ mbta_departures.rb
@@ -5,8 +5,27 @@ require 'pry'
 require_relative 'departure_board.rb'
 
 
+# lateness in seconds, convert to minutes
+# statuses:
+#   On Time, Now Boarding, All Aboard, Delayed
+#   Cancelled, Info to follow, TBD, Late
+#   Arriving, Arrived, Departed, Hold, End
+
 before do
   @departures = DepartureBoard.new.to_hash
+  @timestamp = @departures.first["TimeStamp"].strftime("%I:%M%p %m/%d/%Y")
+
+  @departures.map do |hash|
+    hash.delete("TimeStamp")
+    hash["ScheduledTime"] = hash["ScheduledTime"].strftime("%I:%M%p")
+
+    if hash["Lateness"] = 0
+      hash["Lateness"] = nil
+    elsif hash["Lateness"] > 0
+      hash["Lateness"] /= 60
+    end
+
+  end
 end
 
 get '/' do
