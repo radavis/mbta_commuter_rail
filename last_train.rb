29edbb933e6fb887a303f87bb16b06cedd9c5bf8
Richard Davis <rdavis.bacs@gmail.com>
2013-10-04 17:16:43 -0400

    cleaned up files

--- last_train.rb
+++
@@ -1,9 +0,0 @@
-mbta_files = {
-  gtfs_specs: 'http://www.mbta.com/uploadedfiles/MBTA_GTFS.zip',
-  service_alerts: 'http://developer.mbta.com/lib/GTRTFS/Alerts/Alerts.pb',
-  trip_updates: 'http://developer.mbta.com/lib/gtrtfs/Passages.pb',
-  vehicle_positions: 'http://developer.mbta.com/lib/gtrtfs/Vehicles.pb.',
-
-  rss: 'http://realtime.mbta.com/alertsrss/rssfeed4',
-  rss4: 'http://realtime.mbta.com/alertsrss/rssfeed4',
-}
